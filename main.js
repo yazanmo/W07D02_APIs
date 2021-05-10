49324f0f2a30399d53ba4ed38eb3f67312b48fd8
yazanmo <yazanmostafa96@gmail.com>
2021-05-10 13:34:54 +0300

    practice creat global scope

--- main.js
+++ main.js
@@ -4,13 +4,22 @@ const app = express();
 const port = 3000;
 
 
-app.get("/",(req,res)=>{
+/*app.get("/",(req,res)=>{
     res.send("hello")
 })
 
 app.get("/home",(req,res)=>{
     res.send("home page")
+})*/
+
+const todos = [{ todo: " wake up", isCompleted: false },
+ { todo: "Eat Breakfast", isCompleted: false }];
+
+app.get("/todos",(req,res)=>{
+
+    res.
 })
+
 
 app.listen(port,()=>{
 
