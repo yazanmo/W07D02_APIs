431643a4019708e0a63c105faa8cdc504d9a2e57
yazanmo <yazanmostafa96@gmail.com>
2021-05-10 13:45:07 +0300

    solve branch 3 of practice

--- main.js
+++ main.js
@@ -4,20 +4,14 @@ const app = express();
 const port = 3000;
 
 
-/*app.get("/",(req,res)=>{
-    res.send("hello")
-})
-
-app.get("/home",(req,res)=>{
-    res.send("home page")
-})*/
-
 const todos = [{ todo: " wake up", isCompleted: false },
  { todo: "Eat Breakfast", isCompleted: false }];
 
+ app.use(express.json());
+
 app.get("/todos",(req,res)=>{
 
-    res.
+    res.json(todos)
 })
 
 
